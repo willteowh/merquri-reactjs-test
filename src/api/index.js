0958e710c73e6df523d61f086034be138790828e
willteowh <10542244+willteowh@users.noreply.github.com>
2024-03-22 15:23:27 +0800

    bug fix for API url

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-export const GEO_API_URL_RAW = "http://api.openweathermap.org/geo/1.0/direct";
+export const GEO_API_URL_RAW = "https://api.openweathermap.org/geo/1.0/direct";
 export const GEO_API_URL = `${GEO_API_URL_RAW}?appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`;
 
 export const WEATHER_API_URL_RAW =
